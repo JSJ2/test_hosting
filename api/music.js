529a6f0e92b57270937897921cec68438e2f958b
JSJ2 <josejin215@gmail.com>
2022-10-26 21:39:45 +0900

    5

---
+++ api/music.js
@@ -0,0 +1,110 @@
+let allMusic = [
+    {
+        name : "World's Smallest Violin",
+        artist : "AJR",
+        img : "music-1",
+        audio : "music-1"
+    },
+    {
+        name : "Bang",
+        artist : "AJR",
+        img : "music-2",
+        audio : "music-2"
+    },
+    {
+        name : "100 Bad Days",
+        artist : "AJR",
+        img : "music-3",
+        audio : "music-3"
+    },
+    {
+        name : "Burn The House Down",
+        artist : "AJR",
+        img : "music-4",
+        audio : "music-4"
+    },
+    {
+        name : "Weak",
+        artist : "AJR",
+        img : "music-5",
+        audio : "music-5"
+    },
+    {
+        name : "민물장어의 꿈",
+        artist : "하현우",
+        img : "music-6",
+        audio : "music-6"
+    },
+    {
+        name : "Dangerously",
+        artist : "Charlie Puth",
+        img : "music-7",
+        audio : "music-7"
+    },
+    {
+        name : "시간이 들겠지(feat. Colde(콜드))",
+        artist : "로꼬",
+        img : "music-8",
+        audio : "music-8"
+    },
+    {
+        name : "지칠 때면 (feat. 김종완)",
+        artist : "정준하, 지코",
+        img : "music-9",
+        audio : "music-9"
+    },
+    {
+        name : "사랑은 늘 도망가",
+        artist : "로이킴",
+        img : "music-10",
+        audio : "music-10"
+    },
+    {
+        name : "나의 X에게",
+        artist : "경서",
+        img : "music-11",
+        audio : "music-11"
+    },
+    {
+        name : "다정히 내 이름을 부르면",
+        artist : "경서예지",
+        img : "music-12",
+        audio : "music-12"
+    },
+    {
+        name : "TOMBOY",
+        artist : "혁오",
+        img : "music-13",
+        audio : "music-13"
+    },
+    {
+        name : "비와 당신",
+        artist : "이무진",
+        img : "music-14",
+        audio : "music-14"
+    },
+    {
+        name : "If I Die Tomorrow",
+        artist : "빈지노(Beenzino)",
+        img : "music-15",
+        audio : "music-15"
+    },
+    {
+        name : "In My Mind",
+        artist : "Dynoro",
+        img : "music-16",
+        audio : "music-16"
+    },
+    {
+        name : "More",
+        artist : "Sam Ryder",
+        img : "music-17",
+        audio : "music-17"
+    },
+    {
+        name : "Savage Love [BTS Remix]",
+        artist : "Jawsh 685",
+        img : "music-18",
+        audio : "music-18"
+    },
+]
